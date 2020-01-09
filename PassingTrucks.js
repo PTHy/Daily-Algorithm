c5ac96a83a988aa8f0955b2315b17d6734f67f3a
Park Tae Hyeong <tae020117@gmail.com>
2020-01-09 15:37:46 +0900

    [FIX] 다리를 지나는 트럭 해결

--- PassingTrucks.js
+++ PassingTrucks.js
@@ -1,28 +1,32 @@
+/*
+  프로그래머스 Stack/Queue Level 2
+  다리를 지나는 트럭
+  Javascript
+  https://programmers.co.kr/learn/courses/30/lessons/42583
+*/
+
 function solution(bridge_length, weight, truck_weights) {
-  let count = 0;
+  let count = -1;
   let passingLength = [];
   let passingWeight = 0;
   
-  while (count <= 20) {
-      if (passingWeight < weight) {
+  while (true) {
+      if (passingWeight + truck_weights[count + 1] <= weight) {
+          count++;
           passingWeight += truck_weights[count];
-          count++; 
       }
       
-      for (let i = 0; i < count; i++) {
+      for (let i = 0; i <= count; i++) {
           passingLength[i] = passingLength[i] && passingLength[i] + 1 || 1;
           
-          if (passingLength[i] === bridge_length)
-              passingWeight -= truck_weights[count];
+          if (passingLength[i] === bridge_length) {
+              passingWeight -= truck_weights[i];
+          }
       }
       
       if (passingLength[truck_weights.length - 1] >= bridge_length)
           break;
-      
-      console.log(passingWeight)
   }
 
-  console.log(passingLength)
-  
   return passingLength[0] + 1;
 }
